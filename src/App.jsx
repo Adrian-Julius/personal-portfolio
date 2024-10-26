abda7d918996f359b21f1730d8b9cc2167db2f83
Adrian-Julius <adrianvillaruel2223@gmail.com>
2024-10-26 09:41:45 +0800

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -346,7 +346,7 @@ function App() {
       key={index}
       className={`project h-14 overflow-hidden hover:h-full flex flex-col justify-between text-center gap-5 bg-gradient-to-r from-slate-200 to-slate-300 lg:px-6 py-4 duration-700 rounded-md  shadow-xl ${
         index === imageIndex
-          ? "scale-[1.03] bg-gradient-to-r from-green-300 to-slate-200 "
+          ? "scale-[1.03] bg-gradient-to-r from-green-300 to-slate-200"
           : "scale-100"
       }`}
       onClick={() => {
